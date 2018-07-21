668a7a770dee2dec85dcf06885b884217e145aee
Feihong Hsu <feihong.hsu@gmail.com>
2018-07-20 23:08:06 -0500

    Print 10 random hanzi

--- src/Main.purs
+++ src/Main.purs
@@ -2,12 +2,15 @@ module Main where
 
 import Prelude
 
+import Data.Char (fromCharCode)
+import Data.Maybe (fromMaybe)
+import Data.String.CodeUnits (singleton)
 import Effect (Effect, forE)
-import Effect.Console (log, logShow)
+import Effect.Console (log)
 import Effect.Random (randomInt)
 
 main :: Effect Unit
 main = do
-  forE 1 10 \i -> do 
-    i <- randomInt 1 5
-    logShow i
+  forE 1 10 \_ -> do 
+    i <- randomInt 0x4e00 0x9fff
+    i # fromCharCode # fromMaybe '?' # singleton # log
